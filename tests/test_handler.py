55f9cfa7be2ee6b659ed9d10ac0b6a8a0888de14
Johannes Hoppe <info@johanneshoppe.com>
2018-03-25 17:45:48 +0200

    Add more tests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,13 +1,9 @@
-import pytest
+from unittest.mock import Mock
+
+import httpretty
+from botocore.vendored import requests
 
 from lintipy import Handler
-
-
-@pytest.fixture()
-def handler(sns):
-    hnd = Handler('some linter', 'echo', '1', '2', '3')
-    hnd.event = sns
-    return hnd
 
 
 class TestHandler:
@@ -19,28 +15,52 @@ class TestHandler:
         assert hnd.cmd_args == ('1', '2', '3')
 
     def test_hook(self, handler):
-        assert handler.hook == ['some', 'json']
+        assert handler.hook['installation']['id'] == 234
 
-    def test_sha(self, handler, pull_request_event, push_event):
-        handler._hook = push_event
-        assert handler.sha is None
-
-        handler.event['Records'][0]['Sns']['Subject'] = 'PushEvent'
-        handler._hook = push_event
+    def test_sha(self, handler):
         assert handler.sha == '0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c'
 
-        handler.event['Records'][0]['Sns']['Subject'] = 'PullRequestEvent'
-        handler._hook = pull_request_event
-        assert handler.sha == '0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c'
-
-    def test_code_has_changed(self, handler, pull_request_event, push_event):
-        handler.event['Records'][0]['Sns']['Subject'] = 'PushEvent'
-        handler._hook = push_event
-        assert handler.code_has_changed
-
-        handler.event['Records'][0]['Sns']['Subject'] = 'PullRequestEvent'
-        handler._hook = pull_request_event
+    def test_code_has_changed(self, handler):
         assert handler.code_has_changed
 
     def test_event_type(self, handler):
-        assert handler.event_type == 'TestInvoke'
+        assert handler.event_type in ['PushEvent', 'PullRequestEvent']
+
+    def test_archive_url(self, handler):
+        assert handler.archive_url == (
+            'https://api.github.com/repos/baxterthehacker/public-repo/'
+            'tarball/0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c'
+        )
+
+    def test_statuses_url(self, handler):
+        assert handler.statuses_url == (
+            'https://api.github.com/repos/baxterthehacker/public-repo/'
+            'statuses/0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c'
+        )
+
+    def test_session(self, handler):
+        handler._token = 123
+        assert handler.session
+        assert 'Authorization' in handler.session.headers
+        assert handler.session.headers['Authorization'] == 'token 123'
+
+    def test_full_name(self, handler):
+        handler._session = requests.Session()
+        assert handler.download_code()
+
+    @httpretty.activate
+    def test_call(self, handler):
+        httpretty.register_uri(
+            httpretty.POST, handler.statuses_url,
+            data='',
+            status=201,
+            content_type='application/json',
+        )
+        handler._session = requests.Session()
+        handler._s3 = Mock()
+        handler.download_code = lambda: '.'
+        handler(handler.event, {})
+
+    def test_download_code(self, handler):
+        handler._session = requests.Session()
+        assert handler.download_code()
