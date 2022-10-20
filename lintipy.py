467af277d164a35d5423d9b05c5f728c5ef937f8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 07:30:07 +0000

    Adding tarfile member sanitization to extractall()

--- lintipy.py
+++ lintipy.py
@@ -126,7 +126,26 @@ class DownloadCodeMixin:
             bs.write(response.content)
             bs.seek(0)
             with tarfile.open(fileobj=bs, mode='r:gz') as fs:
-                fs.extractall(path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(fs, path)
             folder = os.listdir(path)[0]
             return os.path.join(path, folder)
 
